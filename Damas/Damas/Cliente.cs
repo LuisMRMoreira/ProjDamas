0703229d804993e1c0d38aaa74564faa7558bd81
Luís Miguel Ribeiro Moreira <lmr_moreira@live.com.pt>
2019-04-11 12:20:42 +0100

    Commit inicial

---
+++ Damas/Damas/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damas
+{
+    public class Cliente : Jogo
+    {//classe representativa de um jogo online (cliente-servidor)
+        private string nomeServidor;
+        private bool autenticacao;
+
+        
+    }
+}
---
+++ Damas/Damas/Invalida.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Damas
+{
+    public class Invalida : Posicao
+    {
+        //Classe necessaria para fazer distinção entre uma posição vazia, invalida e com peça. (ver explicação classe Gestor) 
+
+    }
+}
